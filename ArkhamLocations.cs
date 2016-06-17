891af45579c718b09dc77b5183a35f239a594de8
cflorka <cflorka1@gmail.com>
2016-06-16 19:19:20 -0700

    Updated hierarchy, copy/pasted arkham locations

--- ArkhamLocations.cs
+++ ArkhamLocations.cs
@@ -1,3 +1,30 @@
 namespace Arkham
 {
+Administration Building 	Miskatonic University 	Stable Location 	Money 	Skill 	
+Arkham Asylum 	Downtown 	Stable Location 	Clue 	Sanity 	
+Bank of Arkham 	Downtown 	Stable Location 	Blessing 	Money 	
+Black Cave 	Rivertown 	Unstable Location 	Common Item 	Spell 	
+Curiositie Shoppe 	Northside 	Stable Location 	Common Item 	Unique Item 	
+General Store 	Rivertown 	Stable Location 	Common Item 	Money 	
+Graveyard 	Rivertown 	Unstable Location 	Clue 	Unique Item 	
+Hibb's Roadhouse 	Easttown 	Unstable Location 	Common Item 	Money 	
+Historical Society 	Southside 	Unstable Location 	Skill 	Spell 	
+Independence Square 	Downtown 	Unstable Location 	Clue 	Unique Item 	
+Inner Sanctum 	French Hill 	N/A 	N/A 	N/A 	
+Library 	Miskatonic University 	Stable Location 	Unique Item 	Spell 	
+Ma's Boarding House 	Southside 	Stable Location 	Ally 	Stamina 	
+Newspaper 	Northside 	Stable Location 	Clue 	Money 	
+Police Station 	Easttown 	Stable Location 	Clue 	Common Item 	
+River Docks 	Merchant District 	Stable Location 	Common Item 	Money 	
+Science Building 	Miskatonic University 	Unstable Location 	Clue 	Unique Item 	
+Silver Twilight Lodge 	French Hill 	Unstable Location 	Clue 	Unique Item 	
+South Church 	Southside 	Stable Location 	Blessing 	Sanity 	
+St. Mary's Hospital 	Uptown 	Stable Location 	Clue 	Stamina 	
+The Unnamable 	Merchant District 	Unstable Location 	Clue 	Unique Item 	
+The Witch House 	French Hill 	Unstable Location 	Clue 	Spell 	
+Train Station 	Northside 	Stable Location 	Common Item 	Unique Item 	
+Unvisited Isle 	Merchant District 	Unstable Location 	Clue 	Spell 	
+Velma's Diner 	Easttown
+Woods 	Uptown
+Ye Olde Magick Shoppe 	Uptown
 }
--- Location.cs
+++ Location.cs
@@ -3,41 +3,32 @@ namespace Arkham
 	public class Location
 	{
 		string name;
-		List<Location> connectedLocations;
 		List<Investigator> investigators;
 		List<Monster> monsters;
 		//ToDo clueTokens/events
-		internal Location(string name, List<Location> connectedLocations)
+		internal Location(string name)
 		{
 			this.name = name;
-			this.connectedLocations = connectedLocations
 		}
 		internal void add(Investigator i){investigators.add(i);}
 		internal void add(Monster m){monsters.add(m);}
+		internal List<Location> connectedLocations{ get; set;}
 	}
 
 	public class ArkhamLocation:Location
 	{
-		Location blackArrow, whiteArrow;
-		internal ArkhamLocation(string name, List<Location> connectedLocations,
-			Location blackArrow, Location whiteArrow)
-			:base(name, connectedLocations)
-		{
-			this.blackArrow = blackArrow;
-			this.whiteArrow = whiteArrow;
-		}
+		internal ArkhamLocation(string name):base(name){}
+
+		internal Location blackLocation{ get; set; }
+		internal Location whiteLocation{ get; set; }
 	}
 
 	public class CityLocation:ArkhamLocation
 	{
-		Street connectedStreet;
-		Gate gate;
-		
-		internal ArkhamLocation(string name, Street street)
-			:base(name, new List<Location> {street}, street, street)
-		{
-			connectedStreet = street;
-		}
+		internal ArkhamLocation(string name):base(name){}
+
+		internal Street connectedStreet{ get; set; }
+		internal Gate openGate{ get; set; }
 	}
 	
 	public class Street:ArkhamLocation
